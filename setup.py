ef5e691b9870adcfadc02443d2a2c0f0e0fc875b
Piotr Niewinski <me@piotrniewinski.com>
2023-01-11 13:51:40 +0100

    updated release version to v0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author=             'Piotr Niewinski',
     author_email=       'pioniewinski@gmail.com',
     packages=           find_packages(),
-    version=            'v0.1.1',
+    version=            'v0.2.0',
     install_requires=   get_requirements(),
     license=            'MIT',
     description=        'python tools')
