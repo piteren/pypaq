b7dc28b0e56b96046b82b8a73031393afd6bc069
Piotr Niewinski <me@piotrniewinski.com>
2023-03-07 16:23:07 +0100

    added GradClipperMAVG

--- pypaq/lipytools/moving_average.py
+++ pypaq/lipytools/moving_average.py
@@ -6,14 +6,15 @@ class MovAvg:
 
     def __init__(
             self,
-            factor=     0.1,
-            first_avg=  True,   # first (1/factor/2) values will be averaged
+            factor: float=      0.1,    # (0.0;1.0>
+            first_avg: bool=    True,   # first (1/factor/2) values will be averaged
     ):
         self.value: Optional[float] = None
         self.factor = factor
         self.upd_ix = 0
         self.first_avg = first_avg
         self.firstL = []
+
 
     def upd(self, val:float):
 
@@ -28,10 +29,12 @@ class MovAvg:
 
         return self.value
 
+
     def reset(self, val:Optional[float]=None):
         self.value = val
         self.upd_ix = 0
         self.firstL = []
+
 
     def __call__(self) -> float:
         return self.value
