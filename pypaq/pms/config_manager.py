c4ee7ced2a4d3dcbc56e0a970bc59195597239ae
Piotr Niewinski <me@piotrniewinski.com>
2023-03-11 13:19:00 +0100

    little ConfigManager refactor

--- pypaq/pms/config_manager.py
+++ pypaq/pms/config_manager.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from typing import Optional
 
+from pypaq.lipytools.pylogger import get_pylogger
 from pypaq.lipytools.files import prep_folder, r_json, w_json
 from pypaq.pms.base import POINT
 
@@ -12,10 +13,21 @@ class ConfigManager:
             self,
             file_FP: str,                       # full path to config file
             config: Optional[POINT]=    None,   # {param: value}
-            try_to_load=                True):  # tries to load from file if file exists
+            try_to_load=                True,   # tries to load from file if file exists
+            logger=                     None,
+            loglevel=                   20):
+
+        if not logger:
+            logger = get_pylogger(name='ConfigManager', level=loglevel)
+        self.logger = logger
+
+        self.logger.info(f'*** ConfigManager *** inits, file: {file_FP}')
 
         self.__file = file_FP
-        self.__config: POINT = deepcopy(config) if config is not None else {}
+        self.__config: POINT = {}
+        if config:
+            self.logger.info(f'> initial config: {config}')
+            self.__config = deepcopy(config)
 
         prep_folder(file_FP)
 
@@ -24,35 +36,31 @@ class ConfigManager:
 
         self.__save_file()
 
-    # loads configuration from file and returns it
-    def __read_file(self) -> POINT:
-        return r_json(self.__file)
-
-    # saves configuration to jsonl file
+    # saves configuration to file
     def __save_file(self):
         w_json(self.__config, self.__file)
 
-    # returns (copy of) config
+    # returns (deepcopy of) config
     def get_config(self) -> POINT:
         return deepcopy(self.__config)
 
-    # loads configuration from file, updates self, returns new configuration (from file) or only keys that have changed values
-    def load(self, return_only_changed=True) -> POINT:
+    # loads configuration from file
+    def load(self) -> POINT:
 
-        file_config = self.__read_file() or {}
+        file_config = r_json(self.__file) or {}
         config_changed = {}
         for k in file_config:
             if k not in self.__config or self.__config[k] != file_config[k]:
                 self.__config[k] = file_config[k]
                 config_changed[k] = file_config[k]
 
-        return config_changed if return_only_changed else file_config
+        if config_changed:
+            self.logger.info(f'> loaded new config changes: {config_changed}')
+
+        return self.get_config()
 
     # updates self with given kwargs, saves file if needed
-    def update(
-            self,
-            return_only_changed=    True,
-            **kwargs) -> POINT:
+    def update(self, **kwargs) -> POINT:
 
         config_changed = {}
         for k in kwargs:
@@ -64,6 +72,12 @@ class ConfigManager:
                     self.__config[k] = kwargs[k]
                     config_changed[k] = kwargs[k]
 
-        if config_changed: self.__save_file()
+        if config_changed:
+            self.__save_file()
+            self.logger.info(f'> saved new config changes: {config_changed}')
 
-        return config_changed if return_only_changed else self.get_config()
+        return self.get_config()
+
+    # alias to get_config()
+    def __call__(self, *args, **kwargs):
+        return self.get_config()
