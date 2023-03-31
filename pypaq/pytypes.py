c666c29fdd6d30cb0b0f389f707cf9b632b2e9f1
Piotr Niewinski <me@piotrniewinski.com>
2023-03-31 23:11:43 +0200

    updated types

--- pypaq/pytypes.py
+++ pypaq/pytypes.py
@@ -2,6 +2,6 @@ import numpy as np
 from typing import Union, List
 
 
-NUM = Union[int,float]
+NUM = Union[int,float,np.ndarray] # ~ number
 
-NPL = Union[List[NUM],np.ndarray]
+NPL = Union[List[NUM],np.ndarray] # ~ array of numbers
