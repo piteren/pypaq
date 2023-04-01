0b61df877fc9d660f507b558496866ef46046e4f
Piotr Niewinski <me@piotrniewinski.com>
2023-04-01 21:16:22 +0200

    new ver

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_requirements():
 
 setup(
     name=               'pypaq',
-    version=            'v1.1.0',
+    version=            'v1.1.1',
     url=                'https://github.com/piteren/pypaq.git',
     author=             'Piotr Niewinski',
     author_email=       'pioniewinski@gmail.com',
