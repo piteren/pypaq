1b4d0c5bbe107a92ea361947eb0f67a724f099a2
Piotr Niewinski <me@piotrniewinski.com>
2023-10-03 20:36:15 +0200

    NPL type with Tensor

--- pypaq/pytypes.py
+++ pypaq/pytypes.py
@@ -1,7 +1,8 @@
 import numpy as np
+import torch
 from typing import Union, List
 
 
-NUM = Union[int,float,np.ndarray] # ~ number
+NUM = Union[int, float, np.ndarray] # ~ number
 
-NPL = Union[List[NUM],np.ndarray] # ~ array of numbers
+NPL = Union[List[NUM], np.ndarray, torch.Tensor] # ~ array of numbers
