071ce18bb60eb1219fca7b29ce60f090cc3a35a4
Piotr Niewinski <me@piotrniewinski.com>
2023-10-26 01:59:15 +0200

    ZeroesProcessor with TNS, zeroes -> TNS

--- pypaq/pytypes.py
+++ pypaq/pytypes.py
@@ -3,6 +3,6 @@ import torch
 from typing import Union, List
 
 
-NUM = Union[int, float, np.ndarray] # ~ number
+NUM = Union[int, float, np.ndarray, torch.Tensor] # ~ number
 
 NPL = Union[List[NUM], np.ndarray, torch.Tensor] # ~ array of numbers
