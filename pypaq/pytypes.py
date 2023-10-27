23f8dda27936198f6aab114fc94ad36eda5045b4
Piotr Niewinski <me@piotrniewinski.com>
2023-10-27 22:48:32 +0200

    NPL iterable

--- pypaq/pytypes.py
+++ pypaq/pytypes.py
@@ -1,8 +1,8 @@
 import numpy as np
 import torch
-from typing import Union, List
+from typing import Union, Iterable
 
 
 NUM = Union[int, float, np.ndarray, torch.Tensor] # ~ number
 
-NPL = Union[List[NUM], np.ndarray, torch.Tensor] # ~ array of numbers
+NPL = Union[Iterable[NUM], np.ndarray, torch.Tensor] # ~ array of numbers
