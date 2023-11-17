c2b6a9409d813a9bbd1e17f691285f05d83d43b8
Piotr Niewinski <me@piotrniewinski.com>
2023-11-17 16:21:29 +0100

    upd release script

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+python setup.py sdist bdist_wheel
+python -m twine upload -r pypi dist/*
 rm -rf build
 rm -rf dist
 rm -rf pypaq.egg-info
-python setup.py sdist bdist_wheel
-python -m twine upload -r pypi dist/*
