7a2bd410b501d9ee84eacb4144d31897d193f175
ppp <me@piotrniewinski.com>
2024-05-19 20:33:18 +0200

    Update version to v1.7.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pypaq.lipytools.files import get_requirements
 
 setup(
     name=               'pypaq',
-    version=            'v1.7.6',
+    version=            'v1.7.7',
     url=                'https://github.com/piteren/pypaq.git',
     author=             'Piotr Niewinski',
     author_email=       'pioniewinski@gmail.com',
