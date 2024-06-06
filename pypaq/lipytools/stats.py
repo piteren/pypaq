1b37bba415b452126f8b97b15170d67e7ad27284
pionie <pioniewinski@gmail.com>
2024-06-06 12:03:58 +0200

    upd msmx

--- pypaq/lipytools/stats.py
+++ pypaq/lipytools/stats.py
@@ -1,26 +1,28 @@
 import math
 import numpy as np
 import pandas as pd
+from scipy import stats
 from typing import Dict, Tuple
 
 from pypaq.pytypes import NPL
 
 
-# min, avg, max ..of num list
+# min, avg, max
 def mam(vals:NPL) -> Tuple[float,float,float]:
     if len(vals): return min(vals), sum(vals) / len(vals), max(vals)
     else:         return 0.0, 0.0, 0.0
 
-# mean, std, min, max (from given list of values or np.arr)
+# mean, std, SEM, h95, min, max
 def msmx(vals:NPL) -> Dict:
     arr = np.asarray(vals) if type(vals) is list else vals
     _mean, _std, _min, _max = float(np.mean(arr)), float(np.std(arr)), float(np.min(arr)), float(np.max(arr))
-    _mean_std = _std / math.sqrt(len(vals))
-    ret_dict = {'mean':_mean, 'std':_std, 'mean_std':_mean_std, 'min':_min, 'max':_max}
+    _sem = _std / math.sqrt(len(vals))
+    _h95 = _sem * stats.t.ppf(0.975, len(vals) - 1) # 0.975 is 1 + 0.95 / 2
+    ret_dict = {'mean':_mean, 'std':_std, 'sem':_sem, 'h95':_h95, 'min':_min, 'max':_max}
     ret_dict['string'] = ' '.join([f'{k}:{ret_dict[k]:.5f}' for k in ret_dict])
     return ret_dict
 
-# deep stats (with pandas)
+# some stats with pandas
 def stats_pd(
         val_list :NPL,
         n_percentiles=  10) -> str:
