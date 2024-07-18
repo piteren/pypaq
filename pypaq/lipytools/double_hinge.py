4683934dc7e0b35b4a58f85762989aacfc1227cc
ppp <me@piotrniewinski.com>
2024-07-18 16:00:04 +0200

    new double hinge

--- pypaq/lipytools/double_hinge.py
+++ pypaq/lipytools/double_hinge.py
@@ -5,28 +5,21 @@ from pypaq.exception import PyPaqException
 def double_hinge(
         s_val: float,
         e_val: float,
-        sf: float,
-        ef: float,
+        a: int,
+        b: int,
         counter: int,
-        max_count: int,
 ) -> float:
-    """
-    returns:
-        - s_val in range <0;A> steps
-        - linear interpolation from s_val to e_val in range <A;B>
-        - e_val in range <A;max_count>
+    """ returns:
+    - s_val for counter < a
+    - linear interpolation from s_val to e_val in range <a;b>
+    - e_val for counter > b """
 
-    counter (step) belongs to <0;max_count>
-    A = max_count * sf
-    B = max_count * (1-ef)
-    0 <= A <= B <= max_count
-    """
-    if sf + ef > 1:
-        raise PyPaqException(f'sf + ef cannot be higher than 1')
+    if b < a or b == a and s_val != e_val:
+        raise PyPaqException('bad arguments!')
 
-    x = counter / max_count         # where we are in time (x)
-    y = (x-sf)/(1-sf-ef)            # how high we are
-    if y<0: y=0
-    if y>1: y=1
-    val = s_val + (e_val-s_val)*y   # final value
-    return val
+    if counter < a:
+        return s_val
+    if counter > b:
+        return e_val
+    y = (counter - a) / (b - a)
+    return s_val + (e_val-s_val) * y
--- tests/lipytools/test_double_hinge.py
+++ tests/lipytools/test_double_hinge.py
@@ -8,17 +8,47 @@ class Test_double_hinge(unittest.TestCase):
 
     def test_double_hinge(self):
 
+        v = double_hinge(s_val=0.0, e_val=1.0, a=0, b=10, counter=0)
+        print(v)
+        self.assertTrue(v == 0.0)
+
+        v = double_hinge(s_val=0.0, e_val=1.0, a=0, b=10, counter=10)
+        print(v)
+        self.assertTrue(v == 1.0)
+
+        v = double_hinge(s_val=0.0, e_val=1.0, a=0, b=10, counter=5)
+        print(v)
+        self.assertTrue(v == 0.5)
+
+        v = double_hinge(s_val=0.0, e_val=1.0, a=0, b=10, counter=9)
+        print(v)
+        self.assertTrue(v == 0.9)
+
+        v = double_hinge(s_val=0.0, e_val=1.0, a=0, b=10, counter=-1)
+        print(v)
+        self.assertTrue(v == 0.0)
+
+        v = double_hinge(s_val=0.0, e_val=1.0, a=0, b=10, counter=20)
+        print(v)
+        self.assertTrue(v == 1.0)
+
+        v = double_hinge(s_val=1.0, e_val=0.0, a=0, b=10, counter=1)
+        print(v)
+        self.assertTrue(v == 0.9)
+
         r = 100
         counter = list(range(r))
         vals = [double_hinge(
             s_val=      0.9,
             e_val=      -0.3,
-            sf=         0.1,
-            ef=         0.3,
-            counter=    c,
-            max_count=  r) for c in counter]
+            a=          10,
+            b=          70,
+            counter=    c) for c in counter]
         two_dim(vals)
-
-        v = double_hinge(0.9,-0.3,0.1,0.3,10,100)
-        print(v)
-        self.assertTrue(v==0.9)
+        vals = [double_hinge(
+            s_val=      2,
+            e_val=      5,
+            a=          30,
+            b=          50,
+            counter=    c) for c in counter]
+        two_dim(vals)
