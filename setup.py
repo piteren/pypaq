f7242b1dce68ee888c5c0281592c6aea96c9375e
pionie <pioniewinski@gmail.com>
2024-12-08 23:38:44 +0100

    Update version to v1.10.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pypaq.lipytools.files import get_requirements
 
 setup(
     name=               'pypaq',
-    version=            'v1.10.2',
+    version=            'v1.10.3',
     url=                'https://github.com/piteren/pypaq.git',
     author=             'Piotr Niewinski',
     author_email=       'pioniewinski@gmail.com',
