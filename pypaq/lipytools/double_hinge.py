8a6db316e259e7105dd87ea04b38b0b44f91eec6
pionie <pioniewinski@gmail.com>
2025-05-12 09:18:02 +0200

    more types

--- pypaq/lipytools/double_hinge.py
+++ pypaq/lipytools/double_hinge.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from pypaq.exception import PyPaqException
 
 
@@ -8,18 +10,29 @@ def double_hinge(
         a_point: float,
         b_point: float,
         point, # int, float, np.ndarray
-) -> float:
+):
     """ returns:
     - a_value for point <= a_point
     - b_value for point >= b_point
     - linear interpolation from a_value to b_value in range (a_point;b_point) """
+    r = _double_hinge_np(a_value, b_value, a_point, b_point, point)
+    if type(point) is not np.ndarray:
+        r = float(r)
+    return r
+
+
+def _double_hinge_np(
+        a_value: float,
+        b_value: float,
+        a_point: float,
+        b_point: float,
+        point: np.ndarray,
+) -> np.ndarray:
 
     if b_point < a_point or b_point == a_point and a_value != b_value:
         raise PyPaqException('bad arguments!')
 
-    if point <= a_point:
-        return a_value
-    if point >= b_point:
-        return b_value
-    y = (point - a_point) / (b_point - a_point)
-    return a_value + (b_value - a_value) * y
+    r = (point - a_point) / (b_point - a_point)
+    v_max, v_min = (b_value, a_value) if b_value > a_value else (a_value, b_value)
+    r = np.maximum(v_min, np.minimum(v_max, r))
+    return a_value + (b_value - a_value) * r
