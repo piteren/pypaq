7c681565a72fa405b9a06f5579c28ef61479e267
pionie <pioniewinski@gmail.com>
2025-05-12 09:21:27 +0200

    fixed tests

--- tests/lipytools/test_double_hinge.py
+++ tests/lipytools/test_double_hinge.py
@@ -4,51 +4,51 @@ from pypaq.lipytools.double_hinge import double_hinge
 from pypaq.lipytools.plots import two_dim
 
 
-class Test_double_hinge(unittest.TestCase):
+class TestDoubleHinge(unittest.TestCase):
 
     def test_double_hinge(self):
 
-        v = double_hinge(s_val=0.0, e_val=1.0, a=0, b=10, counter=0)
+        v = double_hinge(a_value=0.0, b_value=1.0, a_point=0, b_point=10, point=0)
         print(v)
         self.assertTrue(v == 0.0)
 
-        v = double_hinge(s_val=0.0, e_val=1.0, a=0, b=10, counter=10)
+        v = double_hinge(a_value=0.0, b_value=1.0, a_point=0, b_point=10, point=10)
         print(v)
         self.assertTrue(v == 1.0)
 
-        v = double_hinge(s_val=0.0, e_val=1.0, a=0, b=10, counter=5)
+        v = double_hinge(a_value=0.0, b_value=1.0, a_point=0, b_point=10, point=5)
         print(v)
         self.assertTrue(v == 0.5)
 
-        v = double_hinge(s_val=0.0, e_val=1.0, a=0, b=10, counter=9)
+        v = double_hinge(a_value=0.0, b_value=1.0, a_point=0, b_point=10, point=9)
         print(v)
         self.assertTrue(v == 0.9)
 
-        v = double_hinge(s_val=0.0, e_val=1.0, a=0, b=10, counter=-1)
+        v = double_hinge(a_value=0.0, b_value=1.0, a_point=0, b_point=10, point=-1)
         print(v)
         self.assertTrue(v == 0.0)
 
-        v = double_hinge(s_val=0.0, e_val=1.0, a=0, b=10, counter=20)
+        v = double_hinge(a_value=0.0, b_value=1.0, a_point=0, b_point=10, point=20)
         print(v)
         self.assertTrue(v == 1.0)
 
-        v = double_hinge(s_val=1.0, e_val=0.0, a=0, b=10, counter=1)
+        v = double_hinge(a_value=1.0, b_value=0.0, a_point=0, b_point=10, point=1)
         print(v)
         self.assertTrue(v == 0.9)
 
         r = 100
-        counter = list(range(r))
+        point = list(range(r))
         vals = [double_hinge(
-            s_val=      0.9,
-            e_val=      -0.3,
-            a=          10,
-            b=          70,
-            counter=    c) for c in counter]
+            a_value=    0.9,
+            b_value=   -0.3,
+            a_point=    10,
+            b_point=    70,
+            point=      c) for c in point]
         two_dim(vals)
         vals = [double_hinge(
-            s_val=      2,
-            e_val=      5,
-            a=          30,
-            b=          50,
-            counter=    c) for c in counter]
+            a_value=    2,
+            b_value=    5,
+            a_point=    30,
+            b_point=    50,
+            point=      c) for c in point]
         two_dim(vals)
