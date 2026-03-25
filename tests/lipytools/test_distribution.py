62b2b54b4b149dfba5419cdf619f20a88f2d0331
pionie <pioniewinski@gmail.com>
2026-03-25 13:55:16 +0100

    resample fin

--- tests/lipytools/test_distribution.py
+++ tests/lipytools/test_distribution.py
@@ -1,11 +1,22 @@
-from pypaq.lipytools.plots import histogram
 import numpy as np
+
+from pypaq.lipytools.distribution import sample_to_flatten
+from pypaq.lipytools.plots import histogram
 
 
 def test_sample_to_flatten():
+
     vals = np.random.normal(size=100000)
     histogram(vals)
-    vals_re = sample_to_flatten(vals, n_new=50000)
+
+    vals_re = sample_to_flatten(vals)
     histogram(vals_re)
+
+    vals_conc = np.concatenate((vals, vals_re))
+    histogram(vals_conc)
+
+    vals_re = sample_to_flatten(vals, bins=5, n_new=50000)
+    histogram(vals_re)
+
     vals_conc = np.concatenate((vals, vals_re))
     histogram(vals_conc)
