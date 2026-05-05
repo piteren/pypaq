015674a23bba57471c096436d1798d75cd0f9c58
pionie <pioniewinski@gmail.com>
2026-05-05 14:42:02 +0200

    stdout handler for class logger

--- pypaq/lipytools/pylogger.py
+++ pypaq/lipytools/pylogger.py
@@ -6,7 +6,7 @@ DEFAULT_FMT = '%(asctime)s {%(filename)20s:%(lineno)4d} p%(process)s %(levelname
 
 
 def setup_logging(
-        level: int = logging.INFO,
+        level: int | None = None,
         folder: str | None = None,
         log_file_name: str | None = None,
         to_stdout: bool = True,
@@ -16,7 +16,9 @@ def setup_logging(
     Call once at app startup."""
 
     logger = logging.getLogger()
-    logger.setLevel(level)
+
+    if level is not None:
+        logger.setLevel(level)
 
     formatter = logging.Formatter(fmt)
 
@@ -41,6 +43,7 @@ class Logged:
             self,
             level: int | None = None,
             folder: str | None = None,
+            to_stdout: bool = True,
             fmt: str = DEFAULT_FMT,
     ) -> logging.Logger:
         """Call in class init just after setting self.name,
@@ -55,11 +58,18 @@ class Logged:
         if level is not None:
             logger.setLevel(level)
 
+        formatter = logging.Formatter(fmt)
+
+        has_stream = any(type(h) is logging.StreamHandler for h in logger.handlers)
+        if to_stdout and not has_stream:
+            sh = logging.StreamHandler()
+            sh.setFormatter(formatter)
+            logger.addHandler(sh)
+
         # prevents duplicated FileHandlers
         has_file = any(isinstance(h, logging.FileHandler) for h in logger.handlers)
         if folder and not has_file:
             prep_folder(folder)
-            formatter = logging.Formatter(fmt)
             fh = logging.FileHandler(f'{folder}/{obj_name or self.__class__.__qualname__}.log')
             fh.setFormatter(formatter)
             logger.addHandler(fh)
