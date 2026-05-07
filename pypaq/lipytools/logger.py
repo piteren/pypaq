eea3ceaa737b80c8721cb21c8749764944687ed0
Piotr Niewinski <piotr.niewinski@a5labs.co>
2026-05-07 20:46:59 +0000

    more logger mod options

--- pypaq/lipytools/logger.py
+++ pypaq/lipytools/logger.py
@@ -5,21 +5,29 @@ from pypaq.lipytools.files import prep_folder
 DEFAULT_FMT = '%(asctime)s {%(filename)20s:%(lineno)4d} p%(process)s %(levelname)s: %(message)s'
 
 
-def logging_mod(
-        level: int | None = None,
+def logger_mod(
+        level: int | None = logging.INFO,
+            # handlers
         folder: str | None = None,
         log_file_name: str | None = None,
         to_stdout: bool = True,
+            # format
         fmt: str = DEFAULT_FMT,
+        file_width: int = 20,
+        enable_process: bool = True,
 ) -> None:
-    """Configure the root logger handlers.
-    Call once at app startup."""
+    """modify the root logger level, handlers and format
+    call once at app startup"""
 
     logger = logging.getLogger()
 
     if level is not None:
         logger.setLevel(level)
 
+    if "(filename)20s" in fmt:
+        fmt = fmt.replace("(filename)20s",f"(filename){file_width}s")
+    if not enable_process and "p%(process)s " in fmt:
+        fmt = fmt.replace("p%(process)s ", "")
     formatter = logging.Formatter(fmt)
 
     has_stream = any(type(h) is logging.StreamHandler for h in logger.handlers)
